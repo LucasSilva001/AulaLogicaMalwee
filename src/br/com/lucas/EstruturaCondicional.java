074d59f8a1dea3ebe9c327127a45d4dfc62486e4
Luquinhassim <Lucasdaraelu@gmail.com>
2021-05-06 16:01:45 -0300

    Laço de repetição WHILE

--- src/br/com/lucas/EstruturaCondicional.java
+++ src/br/com/lucas/EstruturaCondicional.java
@@ -3,18 +3,20 @@ package br.com.lucas;
 public class EstruturaCondicional {
 
 	public static void main(String[] args) {
+		
 		 int idade = 25;
 		 
 		 if(idade < 12) {
 			 System.out.println("Criança");
 			
-		 } else if(idade < 18) {
+		 } else if(idade > 12 && idade < 18) {
 			 System.out.println("Jovem");
 			 
-		 } else if(idade < 65) {
-			 
+		 } else if(idade >= 18 && idade < 65) {
+			 System.out.println("Adulto");
 		 } else {
 			 System.out.println("Idoso"); 
+			 
 		 }
 
 	}
